54f6a715b8f201fbbc86f50cb9b7f3b67f460437
Patrice Lebel <patleb@users.noreply.github.com>
2016-08-01 09:24:39 -0400

    flash messages controller helpers added

--- lib/surface/engine.rb
+++ lib/surface/engine.rb
@@ -1,6 +1,71 @@
 module Surface
   class Engine < ::Rails::Engine
     initializer "surface.configure_application" do
+      ActiveSupport.on_load :action_controller do
+        ::ActionController::Base.class_eval do
+          module WithFlashMessages
+            def redirect_to(options = {}, response_status_and_flash = {})
+              messages = response_status_and_flash[:flash]
+              if messages && (messages.is_a?(Symbol) || messages.is_a?(Array))
+                flashes = {}
+                locals = response_status_and_flash[:locals]
+                if messages.is_a?(Array)
+                  messages.each do |key|
+                    flashes[key] = flash_messages(key, locals)
+                  end
+                else
+                  flashes[messages] = flash_messages(messages, locals)
+                end
+                response_status_and_flash.delete(:locals)
+                response_status_and_flash[:flash] = flashes
+              end
+              super
+            end
+          end
+          prepend WithFlashMessages
+
+          private
+
+          def flash!(*args)
+            options = args.extract_options!
+            args.each do |key|
+              flash[key] = flash_messages(key, options[:locals])
+            end
+            options.except(:locals).each do |key, value|
+              flash[key] = value
+            end
+          end
+
+          def flash_now!(*args)
+            options = args.extract_options!
+            args.each do |key|
+              flash.now[key] = flash_messages(key, options[:locals])
+            end
+            options.except(:locals).each do |key, value|
+              flash.now[key] = value
+            end
+          end
+
+          def flash_messages(key, *args)
+            options = args.extract_options!
+
+            i18n_key = "flash_messages.#{params[:controller]}.#{params[:action]}.#{key}"
+            i18n_key.gsub!(/\//, ".")
+            if I18n.exists?(i18n_key)
+              return I18n.t(i18n_key, options)
+            end
+
+            i18n_key = "flash_messages.defaults.#{params[:action]}.#{key}"
+            if I18n.exists?(i18n_key)
+              return I18n.t(i18n_key, options)
+            end
+
+            i18n_key = "flash_messages.defaults.#{key}"
+            I18n.t(i18n_key, options)
+          end
+        end
+      end
+
       ActiveSupport.on_load :action_view do
         ::ActionView::Helpers::FormBuilder.class_eval do
           module WithCurrentValue
