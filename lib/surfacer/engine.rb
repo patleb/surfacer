c2227d9a9119f0c4851b0517dbf9b3a39b607c85
Patrice Lebel <patleb@users.noreply.github.com>
2016-08-22 18:17:02 -0400

    added Gemfile + requires

--- lib/surfacer/engine.rb
+++ lib/surfacer/engine.rb
@@ -1,12 +1,21 @@
 module Surfacer
   class Engine < ::Rails::Engine
+    # Initialize engine dependencies on wrapper application
+    Gem.loaded_specs["surfacer"].dependencies.each do |d|
+      begin
+        require d.name
+      rescue LoadError => e
+        # Put exceptions here.
+      end
+    end
+
     initializer "surfacer.configure_application" do
       ActiveSupport.on_load :action_view do
         ::ActionView::Helpers::FormBuilder.class_eval do
           module WithCurrentValue
             def range_field(attribute, **options)
               if options.delete(:current)
-                options[:oninput] = options[:onchange] = "this.setAttribute('data-range', this.value);"
+                options[:oninput] = options[:onchange] = "this.setAttribute('data-range', this.value)"
                 options['data-range'] = options[:value] = object.send(attribute) || options[:value]
               end
 
